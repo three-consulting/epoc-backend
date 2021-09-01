bfcd58ee8193775e946f0c36da4cd1ab9a8c4707
Tuomas Tammero <tuomas@three.consulting>
2021-09-01 21:12:30 +0300

    Fix test name

--- src/test/kotlin/three/consulting/epoc/controller/CustomerControllerIntegrationTest.kt
+++ src/test/kotlin/three/consulting/epoc/controller/CustomerControllerIntegrationTest.kt
@@ -8,9 +8,8 @@ import org.springframework.http.HttpStatus
 class CustomerControllerIntegrationTest : ControllerIntegrationTest() {
 
     @Test
-    fun `get customer`() {
+    fun `get customer for id returns 200`() {
         val response = restTemplate.getForEntity("/customer/1", ObjectNode::class.java)
-
         assertThat(response.statusCode).isEqualTo(HttpStatus.OK)
     }
 }
