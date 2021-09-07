db8b58327677f9a7208a4e45fe53c45408f0506a
Ilkka Vuorio <ilkka@three.consulting>
2021-09-07 12:47:39 +0300

    Add jsonPostEntity to test utils for creating post request with json

---
+++ src/test/kotlin/three/consulting/epoc/utils/HttpBuilder.kt
@@ -0,0 +1,12 @@
+package three.consulting.epoc.utils
+
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
+
+fun jsonPostEntity(jsonLocation: String): HttpEntity<String>{
+    val jsonString = jsonReader(jsonLocation)
+    val httpHeaders = HttpHeaders()
+    httpHeaders.contentType = MediaType.APPLICATION_JSON
+    return HttpEntity(jsonString, httpHeaders)
+}
