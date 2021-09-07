49a81fc262b55d442aad7488137298cb16b9a713
Ilkka Vuorio <ilkka@three.consulting>
2021-09-07 13:35:38 +0300

    Add Controller for creating new Customer

--- src/main/kotlin/three/consulting/epoc/controller/CustomerController.kt
+++ src/main/kotlin/three/consulting/epoc/controller/CustomerController.kt
@@ -1,11 +1,10 @@
 package three.consulting.epoc.controller
 
 import org.springframework.http.MediaType.ALL_VALUE
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import three.consulting.epoc.dto.CustomerDTO
 import three.consulting.epoc.service.CustomerService
+import javax.validation.Valid
 
 @RestController
 @RequestMapping(path = ["/customer"])
@@ -13,4 +12,7 @@ class CustomerController(private val customerService: CustomerService) {
 
     @GetMapping(value = ["/{customerId}"], consumes = [ALL_VALUE])
     fun getCustomerForId(@PathVariable customerId: Long) = customerService.findCustomerForId(customerId)
+
+    @PostMapping()
+    fun createCustomer(@Valid @RequestBody customer: CustomerDTO) = customerService.createCustomer(customer)
 }
--- src/test/kotlin/three/consulting/epoc/controller/CustomerControllerIntegrationTest.kt
+++ src/test/kotlin/three/consulting/epoc/controller/CustomerControllerIntegrationTest.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.node.ObjectNode
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.http.HttpStatus
+import three.consulting.epoc.utils.jsonPostEntity
 
 class CustomerControllerIntegrationTest : ControllerIntegrationTest() {
 
@@ -11,5 +12,20 @@ class CustomerControllerIntegrationTest : ControllerIntegrationTest() {
     fun `get customer for id returns 200`() {
         val response = restTemplate.getForEntity("/customer/1", ObjectNode::class.java)
         assertThat(response.statusCode).isEqualTo(HttpStatus.OK)
+    }
+
+    @Test
+    fun `add customer returns 200`() {
+        val httpEntity = jsonPostEntity("src/test/resources/customer/validCreation.json"
+        )
+        val response = restTemplate.postForEntity("/customer", httpEntity, ObjectNode::class.java)
+        assertThat(response.statusCode).isEqualTo(HttpStatus.OK)
+    }
+    @Test
+    fun `add customer without name returns 400`() {
+        val httpEntity = jsonPostEntity("src/test/resources/customer/invalidCreation.json"
+        )
+        val response = restTemplate.postForEntity("/customer", httpEntity, ObjectNode::class.java)
+        assertThat(response.statusCode).isEqualTo(HttpStatus.BAD_REQUEST)
     }
 }
