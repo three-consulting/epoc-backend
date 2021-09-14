3a6e62ddf030b5735e0722c419ac5a8b239535bb
Ilkka Vuorio <ilkka@three.consulting>
2021-09-14 15:54:13 +0300

    fix to reduce boilerplate with proper imports

--- src/test/kotlin/three/consulting/epoc/controller/EmployeeControllerIntegrationTest.kt
+++ src/test/kotlin/three/consulting/epoc/controller/EmployeeControllerIntegrationTest.kt
@@ -1,7 +1,7 @@
 package three.consulting.epoc.controller
 
 import com.fasterxml.jackson.databind.node.ObjectNode
-import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
@@ -13,24 +13,24 @@ class EmployeeControllerIntegrationTest : ControllerIntegrationTest() {
     @Test
     fun `get employee for id returns 200`() {
         val response = restTemplate.getForEntity("/employee/1", ObjectNode::class.java)
-        Assertions.assertThat(response.statusCode).isEqualTo(HttpStatus.OK)
+        assertThat(response.statusCode).isEqualTo(HttpStatus.OK)
     }
 
     @Test
     fun `add employee returns 200`() {
         val httpEntity = jsonPostEntity("src/test/resources/employee/validCreation.json")
         val response = restTemplate.postForEntity("/employee", httpEntity, ObjectNode::class.java)
-        Assertions.assertThat(response.statusCode).isEqualTo(HttpStatus.OK)
+        assertThat(response.statusCode).isEqualTo(HttpStatus.OK)
     }
     @Test
     fun `add employee without name returns 400`() {
         val httpEntity = jsonPostEntity("src/test/resources/employee/invalidCreation.json")
         val response = restTemplate.postForEntity("/employee", httpEntity, ObjectNode::class.java)
-        Assertions.assertThat(response.statusCode).isEqualTo(HttpStatus.BAD_REQUEST)
+        assertThat(response.statusCode).isEqualTo(HttpStatus.BAD_REQUEST)
     }
 
     @Test
-    fun `update employee return 200`() {
+    fun `update employee returns 200`() {
         val httpEntity = jsonPostEntity("src/test/resources/employee/validUpdate.json")
         val response = restTemplate.exchange(
             URI("/employee"),
@@ -38,11 +38,11 @@ class EmployeeControllerIntegrationTest : ControllerIntegrationTest() {
             httpEntity,
             ObjectNode::class.java
         )
-        Assertions.assertThat(response.statusCode).isEqualTo(HttpStatus.OK)
+        assertThat(response.statusCode).isEqualTo(HttpStatus.OK)
     }
 
     @Test
-    fun `update employee without name return 400`() {
+    fun `update employee without name returns 400`() {
         val httpEntity = jsonPostEntity("src/test/resources/employee/invalidUpdate.json")
         val response = restTemplate.exchange(
             URI("/employee"),
@@ -50,28 +50,28 @@ class EmployeeControllerIntegrationTest : ControllerIntegrationTest() {
             httpEntity,
             ObjectNode::class.java
         )
-        Assertions.assertThat(response.statusCode).isEqualTo(HttpStatus.BAD_REQUEST)
+        assertThat(response.statusCode).isEqualTo(HttpStatus.BAD_REQUEST)
     }
 
     @Test
-    fun `delete employee return 204`() {
+    fun `delete employee returns 204`() {
         val response = restTemplate.exchange(
             URI("/employee/1"),
             HttpMethod.DELETE,
             null,
             ObjectNode::class.java
         )
-        Assertions.assertThat(response.statusCode).isEqualTo(HttpStatus.OK)
+        assertThat(response.statusCode).isEqualTo(HttpStatus.OK)
     }
 
     @Test
-    fun `delete without path id return 400`() {
+    fun `delete without path id returns 400`() {
         val response = restTemplate.exchange(
             URI("/employee"),
             HttpMethod.DELETE,
             null,
             ObjectNode::class.java
         )
-        Assertions.assertThat(response.statusCode).isEqualTo(HttpStatus.METHOD_NOT_ALLOWED)
+        assertThat(response.statusCode).isEqualTo(HttpStatus.METHOD_NOT_ALLOWED)
     }
 }
