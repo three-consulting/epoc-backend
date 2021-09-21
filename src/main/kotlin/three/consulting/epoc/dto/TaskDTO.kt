7ef7e0007f33729b73e268410dbf1a2f17c545ac
Ilkka Vuorio <ilkka@three.consulting>
2021-09-21 11:55:05 +0300

    add task entity

---
+++ src/main/kotlin/three/consulting/epoc/dto/TaskDTO.kt
@@ -0,0 +1,28 @@
+package three.consulting.epoc.dto
+
+import three.consulting.epoc.entity.Task
+import java.time.LocalDate
+import java.time.LocalDateTime
+import javax.validation.constraints.NotBlank
+
+data class TaskDTO(
+    val id: Long? = null,
+    @field:NotBlank val name: String,
+    val description: String? = null,
+    val startDate: LocalDate,
+    val endDate: LocalDate,
+    val project: ProjectDTO,
+    val created: LocalDateTime? = null,
+    val updated: LocalDateTime? = null,
+) {
+    constructor(task: Task) : this (
+        id = task.id,
+        name = task.name,
+        description = task.description,
+        startDate = task.startDate,
+        endDate = task.endDate,
+        project = ProjectDTO(task.project),
+        created = task.created,
+        updated = task.updated,
+    )
+}
---
+++ src/main/kotlin/three/consulting/epoc/entity/Task.kt
@@ -0,0 +1,30 @@
+package three.consulting.epoc.entity
+
+import three.consulting.epoc.dto.TaskDTO
+import java.time.LocalDate
+import java.time.LocalDateTime
+import javax.persistence.*
+
+@Entity
+data class Task(
+    @ManyToOne @JoinColumn(name = "project_id", nullable = false) val project: Project,
+    @field:Column(name = "name", nullable = false) val name: String,
+    @field:Column(name = "description", nullable = true) val description: String? = null,
+    @field:Column(name = "start_date", nullable = false) val startDate: LocalDate = LocalDate.now(),
+    @field:Column(name = "end_date", nullable = true) val endDate: LocalDate = LocalDate.now(),
+    @field:Column(name = "created", nullable = false) val created: LocalDateTime = LocalDateTime.now(),
+    @field:Column(name = "updated", nullable = false) val updated: LocalDateTime = LocalDateTime.now(),
+    @field:Id @field:Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null
+) {
+    constructor(taskDTO: TaskDTO) : this (
+        id = taskDTO.id,
+        name = taskDTO.name,
+        description = taskDTO.description,
+        startDate = taskDTO.startDate,
+        endDate = taskDTO.endDate,
+        created = taskDTO.created ?: LocalDateTime.now(),
+        project = Project(taskDTO.project),
+    )
+}
