aa0b2c4ee96733bb31fc42809584b9f71fe7d655
Oskari Kerppo <=>
2021-11-02 16:51:23 +0200

    added consumes, produces values for controller methods in CustomerController

--- src/main/kotlin/three/consulting/epoc/controller/CustomerController.kt
+++ src/main/kotlin/three/consulting/epoc/controller/CustomerController.kt
@@ -1,6 +1,7 @@
 package three.consulting.epoc.controller
 
 import org.springframework.http.MediaType.ALL_VALUE
+import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 import org.springframework.web.bind.annotation.*
 import three.consulting.epoc.dto.CustomerDTO
 import three.consulting.epoc.service.CustomerService
@@ -10,18 +11,18 @@ import javax.validation.Valid
 @RequestMapping(path = ["/customer"])
 class CustomerController(private val customerService: CustomerService) {
 
-    @GetMapping(value = ["/{customerId}"], consumes = [ALL_VALUE])
+    @GetMapping(value = ["/{customerId}"], consumes = [ALL_VALUE], produces = [APPLICATION_JSON_VALUE])
     fun getCustomerForId(@PathVariable customerId: Long) = customerService.findCustomerForId(customerId)
 
-    @PostMapping
+    @PostMapping(consumes = [APPLICATION_JSON_VALUE], produces = [APPLICATION_JSON_VALUE])
     fun createCustomer(@Valid @RequestBody customer: CustomerDTO) = customerService.createCustomer(customer)
 
-    @PutMapping
+    @PutMapping(consumes = [APPLICATION_JSON_VALUE], produces = [APPLICATION_JSON_VALUE])
     fun updateCustomerForId(@Valid @RequestBody customer: CustomerDTO) = customerService.updateCustomerForId(customer)
 
-    @DeleteMapping(value = ["/{customerId}"])
+    @DeleteMapping(value = ["/{customerId}"], consumes = [ALL_VALUE], produces = [])
     fun deleteCustomerForId(@PathVariable customerId: Long) = customerService.deleteCustomer(customerId)
 
-    @GetMapping
+    @GetMapping(consumes = [ALL_VALUE], produces = [APPLICATION_JSON_VALUE])
     fun getAllCustomers() = customerService.findAllCustomers()
 }
