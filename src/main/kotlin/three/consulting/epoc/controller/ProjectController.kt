d7835ac7472c3d894fbae494d5a3f4759effd595
Oskari Kerppo <=>
2021-11-03 10:25:32 +0200

    Added consumes/produces for ProjectController

--- src/main/kotlin/three/consulting/epoc/controller/ProjectController.kt
+++ src/main/kotlin/three/consulting/epoc/controller/ProjectController.kt
@@ -1,5 +1,7 @@
 package three.consulting.epoc.controller
 
+import org.springframework.http.MediaType.ALL_VALUE
+import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 import org.springframework.web.bind.annotation.*
 import three.consulting.epoc.dto.ProjectDTO
 import three.consulting.epoc.service.ProjectService
@@ -9,18 +11,18 @@ import javax.validation.Valid
 @RequestMapping(path = ["/project"])
 class ProjectController(private val projectService: ProjectService) {
 
-    @GetMapping(value = ["/{projectId}"])
+    @GetMapping(value = ["/{projectId}"], consumes = [ALL_VALUE], produces = [APPLICATION_JSON_VALUE])
     fun getProjectForId(@PathVariable projectId: Long) = projectService.findProjectForId(projectId)
 
-    @PostMapping
+    @PostMapping(consumes = [APPLICATION_JSON_VALUE], produces = [APPLICATION_JSON_VALUE])
     fun createProject(@Valid @RequestBody project: ProjectDTO) = projectService.createProject(project)
 
-    @PutMapping
+    @PutMapping(consumes = [APPLICATION_JSON_VALUE], produces = [APPLICATION_JSON_VALUE])
     fun updateProjectForId(@Valid @RequestBody project: ProjectDTO) = projectService.updateProjectForId(project)
 
-    @DeleteMapping(value = ["/{projectId}"])
+    @DeleteMapping(value = ["/{projectId}"], consumes = [ALL_VALUE], produces = [])
     fun deleteProjectForId(@PathVariable projectId: Long) = projectService.deleteProject(projectId)
 
-    @GetMapping
+    @GetMapping(consumes = [ALL_VALUE], produces = [APPLICATION_JSON_VALUE])
     fun getAllProjects() = projectService.findAllProjects()
 }
