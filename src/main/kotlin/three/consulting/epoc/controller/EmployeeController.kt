8ad12b9e4dbeaee76f943c910cfa1cb8bcb793b6
Oskari Kerppo <=>
2021-11-03 10:15:17 +0200

    added consumes/produces values to EmployeeController

--- src/main/kotlin/three/consulting/epoc/controller/EmployeeController.kt
+++ src/main/kotlin/three/consulting/epoc/controller/EmployeeController.kt
@@ -1,5 +1,7 @@
 package three.consulting.epoc.controller
 
+import org.springframework.http.MediaType.ALL_VALUE
+import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 import org.springframework.web.bind.annotation.*
 import three.consulting.epoc.dto.EmployeeDTO
 import three.consulting.epoc.service.EmployeeService
@@ -9,18 +11,18 @@ import javax.validation.Valid
 @RequestMapping(path = ["/employee"])
 class EmployeeController(private val employeeService: EmployeeService) {
 
-    @GetMapping(value = ["/{employeeId}"])
+    @GetMapping(value = ["/{employeeId}"], consumes = [ALL_VALUE], produces = [APPLICATION_JSON_VALUE])
     fun getEmployeeForId(@PathVariable employeeId: Long) = employeeService.findEmployeeForId(employeeId)
 
-    @PostMapping
+    @PostMapping(consumes = [APPLICATION_JSON_VALUE], produces = [APPLICATION_JSON_VALUE])
     fun createEmployee(@Valid @RequestBody employee: EmployeeDTO) = employeeService.createEmployee(employee)
 
-    @PutMapping
+    @PutMapping(consumes = [APPLICATION_JSON_VALUE], produces = [APPLICATION_JSON_VALUE])
     fun updateEmployeeForId(@Valid @RequestBody employee: EmployeeDTO) = employeeService.updateEmployeeForId(employee)
 
-    @DeleteMapping(value = ["/{employeeId}"])
+    @DeleteMapping(value = ["/{employeeId}"], consumes = [ALL_VALUE], produces = [])
     fun deleteEmployeeForId(@PathVariable employeeId: Long) = employeeService.deleteEmployee(employeeId)
 
-    @GetMapping
+    @GetMapping(consumes = [ALL_VALUE], produces = [APPLICATION_JSON_VALUE])
     fun getAllEmployees() = employeeService.findAllEmployees()
 }
