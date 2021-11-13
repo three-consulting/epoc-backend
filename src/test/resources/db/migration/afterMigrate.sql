067c3809c53d2ee1e203c116b43d65ec033ece93
Tuomas Tammero <tuomas@three.consulting>
2021-11-13 13:50:32 +0200

    More sql formatting fixes

--- src/test/resources/db/migration/afterMigrate.sql
+++ src/test/resources/db/migration/afterMigrate.sql
@@ -7,11 +7,11 @@ INSERT INTO employee( first_name, last_name, email, start_date, created, updated
     ('Test', 'Worker', 'test@worker.fi', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
 INSERT INTO project(customer_id, employee_id, name, description, start_date, status, created, updated) VALUES
-    (1, 1, 'test', 'testing', CURRENT_DATE, 'ACTIVE', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP ),
+    (1, 1, 'test', 'testing', CURRENT_DATE, 'ACTIVE', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
     (1, 1, 'test2', 'testing delete', CURRENT_DATE, 'INACTIVE', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
 INSERT INTO timesheet(project_id, employee_id, name, description, allocation, created, updated) VALUES
-    (1, 1, 'test', 'testing', 100, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP ),
+    (1, 1, 'test', 'testing', 100, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
     (1,1,'test2', 'testing delete', 100, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
 INSERT INTO task(project_id, name, description, start_date, created, updated) VALUES
