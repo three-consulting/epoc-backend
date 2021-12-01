4ee1d2c66502555ea39046b36433cfbcbe3f79fd
Tuomas Tammero <tuomas@three.consulting>
2021-12-01 15:05:58 +0200

    Use custom security config on specific profile

--- src/main/kotlin/three/consulting/epoc/config/SecurityConfig.kt
+++ src/main/kotlin/three/consulting/epoc/config/SecurityConfig.kt
@@ -1,9 +1,11 @@
 package three.consulting.epoc.config
 
+import org.springframework.context.annotation.Profile
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 
+@Profile("default")
 @EnableWebSecurity
 class SecurityConfig : WebSecurityConfigurerAdapter() {
 
--- src/test/kotlin/three/consulting/epoc/controller/ControllerIntegrationTest.kt
+++ src/test/kotlin/three/consulting/epoc/controller/ControllerIntegrationTest.kt
@@ -10,11 +10,13 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import three.consulting.epoc.service.*
 
 @ExtendWith(SpringExtension::class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("it")
 @EnableAutoConfiguration(exclude = [ SecurityAutoConfiguration::class ])
 @AutoConfigureTestDatabase
 @AutoConfigureEmbeddedDatabase
